4bf542ee04c62bd94dd337b161abfc388d9476f7
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2024-03-06 23:04:29 -0300

    Suporte modo escuro em componentes

--- Curso_React_NextJS/admin-template/src/pages/components/template/MenuItem.tsx
+++ Curso_React_NextJS/admin-template/src/pages/components/template/MenuItem.tsx
@@ -1,22 +1,33 @@
 import Link from "next/link"
 
 interface MenuItemProps{
-    url: string
+    url?: string
     texto: string
     icone: any
+    className?: string
+    onClick?: (ev: any) => void
 }
 
 export default function MenuItem(props:MenuItemProps){
-    return(
-        <li className={"hover:bg-gray-200"}>
-            <Link legacyBehavior href={props.url}>
-                <a className={"flex flex-col justify-center items-center w-20 h-20"}> 
+    function renderizarLink(){
+        return(
+            <a className={`flex flex-col justify-center items-center w-20 h-20 ${props.className}`}> 
                     {props.icone}
-                    <span className={"text-xs font-light text-gray-800"}>
+                    <span className={"text-xs font-light "}>
                         {props.texto}
                     </span>
                 </a>
+        )
+    }
+    return(
+        <li onClick={props.onClick} className={"hover:bg-gray-300 cursor-pointer dark:hover:bg-gray-950"}>
+            {props.url? ( 
+            <Link legacyBehavior href={props.url}>
+                {renderizarLink()}
             </Link>
+            ) : (
+                renderizarLink()
+            )}
         </li>
     )
 }
--- Curso_React_NextJS/admin-template/src/pages/components/template/MenuLateral.tsx
+++ Curso_React_NextJS/admin-template/src/pages/components/template/MenuLateral.tsx
@@ -1,10 +1,10 @@
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral(){
     return(
-        <aside className="flex flex-col">
+        <aside className="flex flex-col bg-gray-200 text-gray-700 dark:bg-gray-900 dark:text-gray-100">
             <div className={" flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 via-blue-600 to-purple-800"}>
                 <Logo></Logo>
             </div>
@@ -12,6 +12,10 @@ export default function MenuLateral(){
                 <MenuItem url="/" texto="Início" icone={IconeCasa}></MenuItem>
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}></MenuItem>
                 <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino}></MenuItem>
+                
+            </ul>
+            <ul>
+                <MenuItem texto="Sair" icone={IconeSair} onClick={() => console.log("logout")} className={"text-red-600 hover:bg-red-600 hover:text-white"}></MenuItem>
             </ul>
         </aside>
     )
