66c612c4262f37fac276d0ed7b6e7e437f015edc
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2024-03-07 21:31:09 -0300

    iniciando context API

---
+++ Curso_React_NextJS/admin-template/src/pages/data/context/AppContext.tsx
@@ -0,0 +1,16 @@
+import { createContext } from "vm";
+
+const AppContext = createContext({})
+
+export function AppProvider(props:any){
+    return(
+        <AppContext.Provider value={{
+            nome: "Teste Context API"
+        }}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+
+export default AppContext
